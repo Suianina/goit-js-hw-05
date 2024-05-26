1b1fbb51db8ce86beedb3aa5973e1e040b6991dc
Zhanna Suianina <cezyby3@gmail.com>
2024-05-26 18:23:08 +0300

    save

--- js/task-2.js
+++ js/task-2.js
@@ -1,8 +1,6 @@
 `use strict`;
 
-
-
-
+const getUsersWithFriend = (users, friendName) => users.filter(user => user.friends.includes(friendName));
 
 
 
--- js/task-4.js
+++ js/task-4.js
@@ -1,9 +1,7 @@
 `use strict`;
 
-
-
-
-
+const getTotalBalanceByGender = (users, gender) =>
+  users.filter(user => user.gender === gender).reduce((totalBalance, user) => totalBalance + user.balance, 0);
 
 
 
